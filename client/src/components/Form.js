25d3ae524997a8152a3c15d32a9eb1955f6d06c9
Ghienn <hienhine0310@gmail.com>
2023-09-27 10:19:36 +0700

    update

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, DatePicker, Input, Button, Typography } from "antd";
+import { Form, DatePicker, Input, Button, Typography, message } from "antd";
 import { postAbsenceRequest } from "../services/absenceRequest";
 
 const { RangePicker } = DatePicker;
@@ -14,39 +14,56 @@ const rangeConfig = {
   ],
 };
 
-const onFinish = (fieldsValue) => {
-  // Should format date value before submit.
-  const rangeValue = fieldsValue["date"];
-  const reason = fieldsValue["reason"];
-  const values = {
-    "start-date": rangeValue[0].format("YYYY-MM-DD"),
-    "end-date": rangeValue[1].format("YYYY-MM-DD"),
-    reason: reason,
-  };
-  console.log("Received values of form: ", values);
-  postAbsenceRequest().then((values) => {
-    console.log(values);
-  });
-};
-
 const FormField = () => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [form] = Form.useForm();
+
+  const onFinish = (fieldsValue) => {
+    // Should format date value before submit.
+    const rangeValue = fieldsValue["date"];
+    const reason = fieldsValue["reason"];
+    const values = {
+      "start-date": rangeValue[0].format("YYYY-MM-DD"),
+      "end-date": rangeValue[1].format("YYYY-MM-DD"),
+      reason: reason,
+    };
+    console.log("Received values of form: ", values);
+    postAbsenceRequest().then((values) => {
+      console.log(values);
+    });
+    messageApi.open({
+      type: "success",
+      content: "Send Request Successful!",
+    });
+
+    form.resetFields();
+  };
   return (
-    <Form name="form_item_path" layout="vertical" onFinish={onFinish}>
-      <Form.Item label="Employee name:">
-        <Typography>
-          <pre>Pham Gia Hien</pre>
-        </Typography>
-      </Form.Item>
-      <Form.Item name="date" label="RangePicker:" {...rangeConfig}>
-        <RangePicker />
-      </Form.Item>
-      <Form.Item name="reason" label="Reason:">
-        <Input />
-      </Form.Item>
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form>
+    <div>
+      {" "}
+      {contextHolder}
+      <Form
+        form={form}
+        name="form_item_path"
+        layout="vertical"
+        onFinish={onFinish}
+      >
+        <Form.Item label="Employee name:">
+          <Typography>
+            <pre>Pham Gia Hien</pre>
+          </Typography>
+        </Form.Item>
+        <Form.Item name="date" label="RangePicker:" {...rangeConfig}>
+          <RangePicker />
+        </Form.Item>
+        <Form.Item name="reason" label="Reason:">
+          <Input />
+        </Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form>
+    </div>
   );
 };
 export default FormField;
